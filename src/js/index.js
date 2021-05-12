f210e48e6cf12b2d12858225a1a4e60218ccb6e8
nikitaerastov <71673216+nikitaerastov@users.noreply.github.com>
2021-05-12 13:54:55 -0400

    cnjkfeknfie

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,14 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "YOURKEYHERE";
+const key = "e55b052f2549499d90d7516266234395";
+const query = async function () {
+  try {
+    const response = await fetch("");
+    const data = await response.json;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    alert("something went wrong here...");
+  }
+};
